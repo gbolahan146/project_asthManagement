b8bcba9418eee83ccafcd2f0f79a7c765df44a15
gbolahanprojects <gbolahanoduyemi1@gmail.com>
2020-08-06 13:18:12 +0100

    #feat: included route and resolved conflicts

--- lib/core/routes/route_names.dart
+++ lib/core/routes/route_names.dart
@@ -1,4 +1,7 @@
+
+
 import 'package:asthma_management/views/home/homeView.dart';
+import 'package:asthma_management/views/medication_screen/medication_screen.dart';
 import 'package:asthma_management/views/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -6,10 +9,12 @@ class RouteNames {
   ///Route names used through out the app will be specified as static constants here in this format
   static const String home = '/home';
   static const String splashScreen = '/';
+  static const String medicationScreen = 'medicationScreen';
 
   static Map<String, Widget Function(BuildContext)> routes = {
     ///Named routes to be added here in this format
     RouteNames.home: (context) => Home(),
     RouteNames.splashScreen: (context) => SplashScreen(),
+    RouteNames.medicationScreen: (context) => MedicationScreen(),
   };
 }
