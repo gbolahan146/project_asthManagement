580ca902cc452cbb9172bfb41eba4a5178b93745
gbolahanprojects <gbolahanoduyemi1@gmail.com>
2020-08-06 14:17:18 +0100

    #fixed responsiveness

--- lib/views/medication_screen/medication_screen.dart
+++ lib/views/medication_screen/medication_screen.dart
@@ -1,3 +1,4 @@
+import 'package:asthma_management/core/services/responsiveness.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,7 +12,7 @@ class MedicationScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         centerTitle: true,
         title: Text('Medications', style: GoogleFonts.dmSans(
-          fontSize: 20, color: Color(0xff130A38), fontWeight: FontWeight.bold),
+          fontSize: McGyver.textSize(context, 4), color: Color(0xff130A38), fontWeight: FontWeight.bold),
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
@@ -21,42 +22,42 @@ class MedicationScreen extends StatelessWidget {
            BottomNavigationBarItem(
             icon:  Image.asset(
                         'assets/images/home.png',
-                        height: 24,
-                        width:  24,
+                        height: McGyver.rsDoubleH(context, 5),
+                        width: McGyver.rsDoubleW(context, 8),
                       ),
-            title: Text('Home', style: GoogleFonts.dmSans(fontSize: 10,fontWeight: FontWeight.w500, color: Colors.black)),
+            title: Text('Home', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 1.8),fontWeight: FontWeight.w500, color: Colors.black)),
           ),
            BottomNavigationBarItem(
             icon:  Image.asset(
                         'assets/images/plans.png',
-                        height:  24,
-                        width: 24,
+                        height: McGyver.rsDoubleH(context, 5),
+                        width: McGyver.rsDoubleW(context, 8),
                       ),
-            title: Text('Action plan', style: GoogleFonts.dmSans(fontSize: 10,fontWeight: FontWeight.w500, color: Colors.black)),
+            title: Text('Action plan', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 1.8),fontWeight: FontWeight.w500, color: Colors.black)),
           ),
            BottomNavigationBarItem(
             icon:  Image.asset(
                         'assets/images/pills.png',
-                        height: 24,
-                        width: 24,
+                       height: McGyver.rsDoubleH(context, 5),
+                        width: McGyver.rsDoubleW(context, 8),
                       ),
-            title: Text('Medication', style: GoogleFonts.dmSans(fontSize: 10,fontWeight: FontWeight.w500, color: Colors.black),)
+            title: Text('Medication', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 1.8),fontWeight: FontWeight.w500, color: Colors.black),)
           ),
           BottomNavigationBarItem(
             icon:  Image.asset(
                         'assets/images/printer.png',
-                        height: 24,
-                        width: 24,
+                        height: McGyver.rsDoubleH(context, 5),
+                        width: McGyver.rsDoubleW(context, 8),
                       ),
-            title: Text('Peak flow', style: GoogleFonts.dmSans(fontSize: 10,fontWeight: FontWeight.w500, color: Colors.black),)
+            title: Text('Peak flow', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 1.8),fontWeight: FontWeight.w500, color: Colors.black),)
           ),
           BottomNavigationBarItem(
             icon:  Image.asset(
                         'assets/images/user.png',
-                        height: 24,
-                        width: 24,
+                        height: McGyver.rsDoubleH(context, 5),
+                        width: McGyver.rsDoubleW(context, 8),
                       ),
-            title: Text('Profile', style: GoogleFonts.dmSans(fontSize: 10,fontWeight: FontWeight.w500, color: Colors.black),)
+            title: Text('Profile', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 1.8),fontWeight: FontWeight.w500, color: Colors.black),)
           ),
         ],
       ),
@@ -64,11 +65,11 @@ class MedicationScreen extends StatelessWidget {
           padding: const EdgeInsets.only(left:20.0, right:17),
           child: ListView(
                 children: <Widget>[
-                  SizedBox(height: 32),
-                  Text('Your rescue medication', style: GoogleFonts.dmSans(fontSize: 16, fontWeight: FontWeight.w500,color: Color(0xFF30159C)),),
-                  SizedBox(height: 8),
-                  Text('Quick-relief medications taken as needed for relief of symptoms',style: GoogleFonts.dmSans(fontSize: 10, color: Color(0xFF000000))),
-                  SizedBox(height: 17),
+                  SizedBox(height: McGyver.rsDoubleH(context, 3)),
+                  Text('Your rescue medication', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 2.5), fontWeight: FontWeight.w500,color: Color(0xFF30159C)),),
+                  SizedBox(height: McGyver.rsDoubleH(context, 1.5)),
+                  Text('Quick-relief medications taken as needed for relief of symptoms',style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 1.4), color: Color(0xFF000000))),
+                  SizedBox(height: McGyver.rsDoubleH(context, 2)),
                   Container(
                      padding: EdgeInsets.symmetric(vertical: 8),
                     decoration: BoxDecoration(
@@ -77,13 +78,13 @@ class MedicationScreen extends StatelessWidget {
               ),
                     child: Row(
                       children: <Widget>[
-                        SizedBox(width: 10,),
-                        Container(height: 40,
-                        width: 30,
+                         SizedBox(width: McGyver.rsDoubleW(context, 3),),
+                        Container(height: McGyver.rsDoubleH(context, 6),
+                        width: McGyver.rsDoubleW(context,9),
                           child: Image(image: AssetImage('assets/images/inhaler1.png', ),)
                           ),
-                        SizedBox(width: 20,),
-                        Text('Proair HFA', style: GoogleFonts.dmSans(fontSize: 16, color: Color(0xff0E1633), fontWeight: FontWeight.w500)),
+                        SizedBox(width: McGyver.rsDoubleW(context, 6),),
+                        Text('Proair HFA', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 2.5), color: Color(0xff0E1633), fontWeight: FontWeight.w500)),
                       ],
                     ),
                   ),
@@ -97,13 +98,13 @@ class MedicationScreen extends StatelessWidget {
               ),
                     child: Row(
                       children: <Widget>[
-                        SizedBox(width: 10,),
-                       Container(height: 40,
-                        width: 30,
+                         SizedBox(width: McGyver.rsDoubleW(context, 3),),
+                       Container(height: McGyver.rsDoubleH(context, 6),
+                        width: McGyver.rsDoubleW(context,9),
                           child: Image(image: AssetImage('assets/images/inhaler2.png', ),)
                           ),
-                        SizedBox(width: 20,),
-                        Text('Prov', style: GoogleFonts.dmSans(fontSize: 16, color: Color(0xff0E1633), fontWeight: FontWeight.w500)),
+                        SizedBox(width: McGyver.rsDoubleW(context, 6),),
+                        Text('Prov', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 2.5), color: Color(0xff0E1633), fontWeight: FontWeight.w500)),
                       ],
                     ),
                   ),
@@ -115,17 +116,17 @@ class MedicationScreen extends StatelessWidget {
               ),
                     child: Row(
                       children: <Widget>[
-                        SizedBox(width: 10,),
-                       Container(height: 40,
-                        width: 30,
+                        SizedBox(width: McGyver.rsDoubleW(context, 3),),
+                       Container(height: McGyver.rsDoubleH(context, 6),
+                        width: McGyver.rsDoubleW(context,9),
                           child: Image(image: AssetImage('assets/images/inhaler3.png', ),)
                           ),
-                        SizedBox(width: 20,),
-                        Text('Ventolin HFA', style: GoogleFonts.dmSans(fontSize: 16, color: Color(0xff0E1633), fontWeight: FontWeight.w500)),
+                        SizedBox(width: McGyver.rsDoubleW(context, 6),),
+                        Text('Ventolin HFA', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 2.5), color: Color(0xff0E1633), fontWeight: FontWeight.w500)),
                       ],
                     ),
                   ),
-                  SizedBox(height:27),
+                  SizedBox(height: McGyver.rsDoubleH(context, 3)),
                    Row(
                      mainAxisAlignment: MainAxisAlignment.end,
                      children: <Widget>[
@@ -134,7 +135,6 @@ class MedicationScreen extends StatelessWidget {
                                   color: Color(0xff130A38),
                                   child: MaterialButton(
                                      padding: EdgeInsets.all(10),
-                                    
                                     onPressed: null,
                                     child: Row(
                                       mainAxisAlignment: MainAxisAlignment.center,
@@ -153,27 +153,23 @@ class MedicationScreen extends StatelessWidget {
                                               style: GoogleFonts.dmSans(
                                                   textStyle: TextStyle(
                                                       color: Colors.white,
-                                                      fontSize: 12),
+                                                      fontSize: McGyver.textSize(context, 1.7)),
                                                   fontWeight: FontWeight.bold),
                                               textAlign: TextAlign.center,
                                             ),
                                           ],
                                         ),
-                                        SizedBox(
-                                          width: 5,
-                                        ),
-                                        
                                       ],
                                     ),
                                   ),
                                 ),
                      ],
                    ),
-                  SizedBox(height:24),
-                   Text('Your controller medication', style: GoogleFonts.dmSans(fontSize: 16, fontWeight: FontWeight.w500,color: Color(0xFF30159C)),),
-                  SizedBox(height: 8),
-                  Text('Only use when you having trouble breathing ',style: GoogleFonts.dmSans(fontSize: 10, color: Color(0xFF000000))),
-                  SizedBox(height: 17),
+                  SizedBox(height: McGyver.rsDoubleH(context, 2)),
+                   Text('Your controller medication', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 2.5), fontWeight: FontWeight.w500,color: Color(0xFF30159C)),),
+                  SizedBox(height: McGyver.rsDoubleH(context, 1.5)),
+                  Text('Only use when you having trouble breathing ',style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 1.4), color: Color(0xFF000000))),
+                  SizedBox(height: McGyver.rsDoubleH(context, 2)),
                    Container(
                       padding: EdgeInsets.symmetric(vertical: 8),
                     decoration: BoxDecoration(
@@ -182,13 +178,13 @@ class MedicationScreen extends StatelessWidget {
               ),
                     child: Row(
                       children: <Widget>[
-                        SizedBox(width: 10,),
-                        Container(height: 40,
-                        width: 30,
+                        SizedBox(width: McGyver.rsDoubleW(context, 3),),
+                        Container(height: McGyver.rsDoubleH(context, 6),
+                        width: McGyver.rsDoubleW(context,9),
                           child: Image(image: AssetImage('assets/images/inhaler4.png', ),)
                           ),
-                        SizedBox(width: 20,),
-                        Text('Flovent HFA', style: GoogleFonts.dmSans(fontSize: 16, color: Color(0xff0E1633), fontWeight: FontWeight.w500)),
+                        SizedBox(width: McGyver.rsDoubleW(context, 6),),
+                        Text('Flovent HFA', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 2.5), color: Color(0xff0E1633), fontWeight: FontWeight.w500)),
                       ],
                     ),
                   ),
@@ -202,13 +198,13 @@ class MedicationScreen extends StatelessWidget {
               ),
                     child: Row(
                       children: <Widget>[
-                        SizedBox(width: 10,),
-                        Container(height: 40,
-                        width: 30,
+                        SizedBox(width: McGyver.rsDoubleW(context, 3),),
+                        Container(height: McGyver.rsDoubleH(context, 6),
+                        width: McGyver.rsDoubleW(context,9),
                           child: Image(image: AssetImage('assets/images/inhaler5.png', ),)
                           ),
-                        SizedBox(width: 20,),
-                        Text('Advair', style: GoogleFonts.dmSans(fontSize: 16, color: Color(0xff0E1633), fontWeight: FontWeight.w500)),
+                        SizedBox(width: McGyver.rsDoubleW(context, 6),),
+                        Text('Advair', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 2.5), color: Color(0xff0E1633), fontWeight: FontWeight.w500)),
                       ],
                     ),
                   ),
@@ -220,26 +216,25 @@ class MedicationScreen extends StatelessWidget {
               ),
                     child: Row(
                       children: <Widget>[
-                        SizedBox(width: 10,),
-                        Container(height: 40,
-                        width: 30,
+                        SizedBox(width: McGyver.rsDoubleW(context, 3),),
+                        Container(height: McGyver.rsDoubleH(context, 6),
+                        width: McGyver.rsDoubleW(context,9),
                           child: Image(image: AssetImage('assets/images/inhaler6.png', ),)
                           ),
-                        SizedBox(width: 20,),
-                        Text('Flixotide', style: GoogleFonts.dmSans(fontSize: 16, color: Color(0xff0E1633), fontWeight: FontWeight.w500)),
+                        SizedBox(width: McGyver.rsDoubleW(context, 6),),
+                        Text('Flixotide', style: GoogleFonts.dmSans(fontSize: McGyver.textSize(context, 2.5), color: Color(0xff0E1633), fontWeight: FontWeight.w500)),
                       ],
                     ),
                   ),
-                  SizedBox(height: 27.0,),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            Material(
+                  SizedBox(height: McGyver.rsDoubleH(context, 3)),
+                         Row(
+                     mainAxisAlignment: MainAxisAlignment.end,
+                     children: <Widget>[
+                       Material(
                                   borderRadius: BorderRadius.circular(4),
                                   color: Color(0xff130A38),
                                   child: MaterialButton(
-                                    // padding: EdgeInsets.all(1),
-                                    minWidth: 130,
+                                     padding: EdgeInsets.all(10),
                                     onPressed: null,
                                     child: Row(
                                       mainAxisAlignment: MainAxisAlignment.center,
@@ -257,24 +252,21 @@ class MedicationScreen extends StatelessWidget {
                                               "Add controller medications",
                                               style: GoogleFonts.dmSans(
                                                   textStyle: TextStyle(
-                                                      
                                                       color: Colors.white,
-                                                      fontSize: 12),
+                                                      fontSize: McGyver.textSize(context, 1.7)),
                                                   fontWeight: FontWeight.bold),
                                               textAlign: TextAlign.center,
                                             ),
                                           ],
                                         ),
-                                        SizedBox(
-                                          width: 5,
-                                        ),
+                                        
                                         
                                       ],
                                     ),
                                   ),
                                 ),
-                          ],
-                        ),
+                     ],
+                   ),
 
                 ],
             ),
@@ -282,58 +274,3 @@ class MedicationScreen extends StatelessWidget {
       );
   }
 }
-
-// class BottomNav extends StatelessWidget {
-//   BuildContext context;
-//     int currentIndex ;
-//   BottomNav({@required this.context,this.currentIndex}  );
-//   @override
-//   Widget build(BuildContext context) {
-//     return BottomNavigationBar(
-//               backgroundColor: Color(0XFF6F2184),
-//             currentIndex: currentIndex, // new
-//         items: [
-//            BottomNavigationBarItem(
-//             icon:  Image.asset(
-//                         'images/home.png',
-//                         height: 24,
-//                         width:  24,
-//                       ),
-//             title: Text('Home', style: GoogleFonts.dmSans(fontSize: 10,fontWeight: FontWeight.w500, color: Colors.black)),
-//           ),
-//            BottomNavigationBarItem(
-//             icon:  Image.asset(
-//                         'images/plans.png',
-//                         height:  24,
-//                         width: 24,
-//                       ),
-//             title: Text('Action plan', style: GoogleFonts.dmSans(fontSize: 10,fontWeight: FontWeight.w500, color: Colors.black)),
-//           ),
-//            BottomNavigationBarItem(
-//             icon:  Image.asset(
-//                         'images/pills.png',
-//                         height: 24,
-//                         width: 24,
-//                       ),
-//             title: Text('Medication', style: GoogleFonts.dmSans(fontSize: 10,fontWeight: FontWeight.w500, color: Colors.black),)
-//           ),
-//           BottomNavigationBarItem(
-//             icon:  Image.asset(
-//                         'images/printer.png',
-//                         height: 24,
-//                         width: 24,
-//                       ),
-//             title: Text('Peak flow', style: GoogleFonts.dmSans(fontSize: 10,fontWeight: FontWeight.w500, color: Colors.black),)
-//           ),
-//           BottomNavigationBarItem(
-//             icon:  Image.asset(
-//                         'images/user.png',
-//                         height: 24,
-//                         width: 24,
-//                       ),
-//             title: Text('Profile', style: GoogleFonts.dmSans(fontSize: 10,fontWeight: FontWeight.w500, color: Colors.black),)
-//           ),
-//         ],
-//       );
-//   }
-// }
